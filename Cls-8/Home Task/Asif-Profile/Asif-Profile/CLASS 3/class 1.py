2c533a8f668d3b4c067789290a317954bd61d4f5
mdasifalam <asif_2192@yahoo.com>
2026-03-15 11:52:15 +0600

    Cls-10

---
+++ Cls-8/Home Task/Asif-Profile/Asif-Profile/CLASS 3/class 1.py
@@ -0,0 +1,27 @@
+name = 'aiatullah'
+age=33
+print('aiatullah',age)
+name='aiatullah'
+age=22
+print("Name :", name ,"age",age)
+name="ontim"
+age=12
+print("Name:",name,"Age",age)
+
+
+name="aiatullah"
+age=44
+print(f"y name is{name} i am{age} year old")
+
+
+name="iatullah"
+age=44
+print("y name is {} my yers old{}".format(name,age))
+
+name=input("Enter your name:")
+print(name)
+
+age=int(input("Enter your age"))
+print(age)
+
+
---
+++ Cls-8/Home Task/Asif-Profile/Asif-Profile/CLASS 3/conditonal.py
@@ -0,0 +1,14 @@
+number = -5
+if number > 0:
+    print("posetive")
+
+else:
+    print("negative")
+
+number= -33
+if number > 0:
+    print("odd")
+
+else :
+    print("even")
+
